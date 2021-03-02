dff9aa46a92b095061b92c3078a68f0e32c2bd2f
matheusmelo995 <matheusmendes.melo@outlook.com>
2021-03-02 16:29:16 -0300

    primeiro commit

---
+++ components/Menu.js
@@ -0,0 +1,10 @@
+function Menu() {
+    return (
+        <div>
+            <a href='/'>Home</a> <br />
+            <a href='/orcamento'>Página de Orçamentos!</a>
+        </div>
+    );
+}
+
+export default Menu;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Menu from '../components/Menu'
+import { Button } from 'reactstrap';
+
+function Home() {
+    return (
+        <div>
+            <Menu />
+            <Button color="warning">Warning</Button>
+        </div>
+    )
+}
+  
+  export default Home;
